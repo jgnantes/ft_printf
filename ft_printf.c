7dae7bb5fef1d35feb046609c71efa28da16c2df
jnantes- <jgnantes99@outlook.com>
2025-11-05 20:16:24 -0300

    v1

--- ft_printf.c
+++ ft_printf.c
@@ -10,4 +10,42 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft/libft.h"
+#include <stdarg.h>
 
+int	print_format(char print, va_list ap);
+
+int	ft_printf(const char *format, ...)
+{
+	int     count;
+	va_list ap;
+
+	va_start(ap, format);
+	count = 0;
+	while (*format != '\0')
+	{
+		if (*format == '%')
+			count += print_format(*++format, ap);
+		else
+			count += ft_putchar_fd(*format, 1);
+		*format++;
+	}
+	va_end(ap);
+	return count;
+}
+
+int print_format(char print, va_list ap)
+{
+	int count;
+
+	count = 0;
+	if (print == 'c')
+		count += ft_putchar_fd(va_arg(ap, int), 1);
+	else if (print == 's')
+		count += ft_putstr_fd(va_arg(ap, char *, 1));
+	else if (print == 'd')
+		count += ft_putnbr_fd(va_arg(ap, int), 1);
+	else if (print == 'x')
+		count += print_hexa(va_arg(ap, unsigned int));
+	return (count);
+}
