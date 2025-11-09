b3be0586960e1ed1299c78a1e56462ec4f88ca62
jgnantes <jgnantes@outlook.com>
2025-11-09 20:05:47 -0300

    Pronto(?)

--- ft_printf.c
+++ ft_printf.c
@@ -14,7 +14,7 @@
 #include <stdarg.h>
 
 static int	print_format(char print, va_list ap);
-static int	print_hexa(unsigned long n, int upper);
+static int	print_hexa(unsigned long n, char specifier);
 static int	ft_putunsnbr_fdr(unsigned int n, int fd);
 
 int	ft_printf(const char *format, ...)
@@ -39,91 +39,101 @@ int	ft_printf(const char *format, ...)
 	return (count);
 }
 
-#include <stdio.h>
+/*#include <stdio.h>
 int	main(void)
 {
-	
-	char *str = NULL;
-	char c = '\0';
+	char c = 'A';
 	int n = -20;
-	ft_printf("int n = -20\n\n");
-	ft_printf("Decimal: %d\n", n);
-	printf("Decimal: %d\n", n);
-	ft_printf("Integer: %i\n", n);
-	printf("Integer: %i\n", n);
-	ft_printf("Unsigned: %u\n", n);
-	printf("Unsigned: %u\n", n);
-	ft_printf("Hexadecimal lowercase: %x\n", n);
-	printf("Hexadecimal lowercase: %x\n", n);
-	ft_printf("Hexadecimal uppercase: %X\n", n);
-	printf("Hexadecimal uppercase: %X\n", n);
-	ft_printf("Pointer: %p\n", &n);
-	printf("Pointer: %p\n", &n);
-	ft_printf("Character: %c\n", c);
-	printf("Character: %c\n", c);
-	ft_printf("String: %s\n", str);
-	printf("String: %s\n", str);
-	ft_printf("Percent sign: %%\n");
-	printf("Percent sign: %%\n\n");
+
+	ft_printf("FT Decimal: %d\n", n);
+	printf("OG Decimal: %d\n", n);
+
+	ft_printf("FT Integer: %i\n", n);
+	printf("OG Integer: %i\n", n);
+
+	ft_printf("FT Unsigned: %u\n", n);
+	printf("OG Unsigned: %u\n", n);
+
+	ft_printf("FT Hexadecimal lowercase: %x\n", n);
+	printf("OG Hexadecimal lowercase: %x\n", n);
+
+	ft_printf("FT Hexadecimal uppercase: %X\n", n);
+	printf("OG Hexadecimal uppercase: %X\n", n);
+
+	ft_printf("FT Pointer: %p\n", &n);
+	printf("OG Pointer: %p\n", &n);
+
+	ft_printf("FT Character: %c\n", c);
+	printf("OG Character: %c\n", c);
+
+	ft_printf("FT String: %s\n", "Sample string");
+	printf("OG String: %s\n", "Sample string");
+
+	ft_printf("FT Percent sign: %%\n");
+	printf("OG Percent sign: %%\n\n");
 
 	int i = ft_printf("Here are 23 characters\n");
-	ft_printf("%d\n", i);
-	printf("%d\n\n", i);
+	ft_printf("FT Count %d\n", i);
+	printf("OG Count %d\n\n", i);
 
 	char *ptr = NULL;
-	ft_printf("Pointer NULL: %p\n", ptr);
-	printf("Pointer NULL: %p\n", ptr);
+	ft_printf("FT Pointer NULL: %p\n", ptr);
+	printf("OG Pointer NULL: %p\n", ptr);
+	ft_printf("FT String NULL: %s\n", ptr);
+	printf("OG String NULL: %s\n", ptr);
 
 	//ft_printf("Percent test 100%     \n");
 	//printf("Percent test 100%     \n");
 
 	return (0);
-}
+}*/
 
-static int	print_format(char print, va_list ap)
+static int	print_format(char specifier, va_list ap)
 {
 	int	count;
 
 	count = 0;
-	if (print == 'c')
+	if (specifier == 'c')
 		count += ft_putchar_fdr(va_arg(ap, int), 1);
-	else if (print == 's' || print == 'c')
+	else if (specifier == 's')
 		count += ft_putstr_fdr(va_arg(ap, char *), 1);
-	else if (print == 'd' || print == 'i')
+	else if (specifier == 'd' || specifier == 'i')
 		count += ft_putnbr_fdr(va_arg(ap, int), 1);
-	else if (print == 'u')
+	else if (specifier == 'u')
 		count += ft_putunsnbr_fdr(va_arg(ap, unsigned int), 1);
-	else if (print == 'x')
-		count += print_hexa((unsigned long)(va_arg(ap, unsigned int)), 0);
-	else if (print == 'X')
-		count += print_hexa((unsigned long)(va_arg(ap, unsigned int)), 1);
-	else if (print == 'p')
-	{
-		//if (va_arg(ap, void *) == NULL)
-		//	count += ft_putstr_fdr("(nil)", 1);
-		count += ft_putstr_fdr("0x", 1);
-		count += print_hexa((uintptr_t)(va_arg(ap, void *)), 0);
-	}
-	else if (print == '%')
+	else if (specifier == 'x')
+		count += print_hexa((va_arg(ap, unsigned long)), specifier);
+	else if (specifier == 'X')
+		count += print_hexa((va_arg(ap, unsigned long)), specifier);
+	else if (specifier == 'p')
+		count += print_hexa((uintptr_t)(va_arg(ap, void *)), specifier);
+	else if (specifier == '%')
 		count += ft_putchar_fdr('%', 1);
 	return (count);
 }
 
-static int	print_hexa(unsigned long n, int upper)
+static int	print_hexa(unsigned long n, char specifier)
 {
 	int			count;
 	char	*digits;
 
-	if (upper == 1)
+	count = 0;
+	if (specifier == 'p')
+	{
+		if (n == 0)
+			return (ft_putstr_fdr("(nil)", 1));
+		count += ft_putstr_fdr("0x", 1);
+		specifier = 'x';
+	}
+	else if (specifier == 'X')
 		digits = "0123456789ABCDEF";
 	else
 		digits = "0123456789abcdef";
-	count = 0;
 	if (n < 16)
 		count += ft_putchar_fdr(digits[n], 1);
 	else
 	{
-		count += print_hexa(n / 16, upper);
+		count += print_hexa(n / 16, specifier);
 		count += ft_putchar_fdr(digits[n % 16], 1);
 	}
 	return (count);
